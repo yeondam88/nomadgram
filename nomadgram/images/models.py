377614c20945b68c32bf8d069f9ec58c19179eda
Yeondam Park <giyen9088@gmail.com>
2017-11-10 22:05:41 -0800

    Adding more fields on Admin panel

--- nomadgram/images/models.py
+++ nomadgram/images/models.py
@@ -1,34 +1,52 @@
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 from nomadgram.users import models as user_models
 
+
+@python_2_unicode_compatible
 class TimeStampedModel(models.Model):
-  
-  created_at = models.DateTimeField(auto_now_add=True)
-  updated_at = models.DateTimeField(auto_now=True)
 
-  class Meta:
-    abstract = True
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        abstract = True
+
+
+@python_2_unicode_compatible
 class Image(TimeStampedModel):
-  
-  """ Image Model """
-  
-  file = models.ImageField()
-  location = models.CharField(max_length=140)
-  caption = models.TextField()
-  creator = models.ForeignKey(user_models.User, null=True)
 
+    """ Image Model """
+
+    file = models.ImageField()
+    location = models.CharField(max_length=140)
+    caption = models.TextField()
+    creator = models.ForeignKey(user_models.User, null=True)
+
+    def __str__(self):
+        return '{} - {}'.format(self.location, self.caption)
+
+
+@python_2_unicode_compatible
 class Comment(TimeStampedModel):
-  
-  """ Comment Model """
 
-  message = models.TextField()
-  creator = models.ForeignKey(user_models.User, null=True)
-  image = models.ForeignKey(Image, null=True)
+    """ Comment Model """
 
+    message = models.TextField()
+    creator = models.ForeignKey(user_models.User, null=True)
+    image = models.ForeignKey(Image, null=True)
+
+    def __str__(self):
+        return self.message
+
+
+@python_2_unicode_compatible
 class Like(TimeStampedModel):
-  
-  """ Like Model """
-  
-  creator = models.ForeignKey(user_models.User, null=True)
-  image = models.ForeignKey(Image, null=True)
+
+    """ Like Model """
+
+    creator = models.ForeignKey(user_models.User, null=True)
+    image = models.ForeignKey(Image, null=True)
+
+    def __str__(self):
+        return 'User: {} - Image Caption: {}'.format(self.creator.username, self.image.caption)
