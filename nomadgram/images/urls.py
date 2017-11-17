234dcbc319dd7c76c64ccb40cf1727b5bd389a52
Yeondam Park <giyen9088@gmail.com>
2017-11-16 23:47:53 -0800

    Add url classes

---
+++ nomadgram/images/urls.py
@@ -0,0 +1,21 @@
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    url(
+        regex=r'^all/$',
+        view=views.ListAllImages.as_view(),
+        name='all_image'
+    ),
+    url(
+        regex=r'^comments/$',
+        view=views.ListAllComments.as_view(),
+        name='all_comments'
+    ),
+    url(
+        regex=r'^likes/$',
+        view=views.ListAllLikes.as_view(),
+        name='all_likes'
+    )
+]
