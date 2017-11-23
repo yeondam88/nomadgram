0b6602c767936bdcf59c5d3cfb9aa51fe2e2fc73
Yeondam Park <giyen9088@gmail.com>
2017-11-23 01:23:33 -0800

    update profile

--- nomadgram/images/views.py
+++ nomadgram/images/views.py
@@ -189,6 +189,13 @@ class ModerateComments(APIView):
 
 class ImageDetail(APIView):
 
+    def find_own_image(self, image_id, user):
+        try:
+            image = models.Image.objects.get(id=image_id, creator=user)
+            return image
+        except models.Image.DoesNotExist:
+            return None
+
     def get(self, request, image_id, format=None):
 
         user = request.user
@@ -206,9 +213,9 @@ class ImageDetail(APIView):
 
         user = request.user
 
-        try:
-            image = models.Image.objects.get(id=image_id, creator=user)
-        except models.Image.DoesNotExist:
+        image = self.find_own_image(image_id, user)
+
+        if image is None:
             return Response(status=status.HTTP_401_UNAUTHORIZED)
 
         serializer = serializers.InputImageSerializer(
@@ -223,3 +230,17 @@ class ImageDetail(APIView):
         else:
 
             return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, image_id, format=None):
+
+        user = request.user
+
+        image = self.find_own_image(image_id, user)
+
+        if image is None:
+
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+
+        image.delete()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
