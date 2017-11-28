b46408cf4dc38fabda2452cf6f16cb57ddb39c5b
Yeondam Park <giyen9088@gmail.com>
2017-11-28 00:11:14 -0800

    add redux devtools, i18n

--- frontend/src/redux/configureStore.js
+++ frontend/src/redux/configureStore.js
@@ -1,14 +1,37 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
+import { routerReducer, routerMiddleware } from "react-router-redux";
+import createHistory from "history/createBrowserHistory";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { i18nState } from "redux-i18n";
 import users from "redux/modules/users";
 
-const middlewares = [thunk];
+const env = process.env.NODE_ENV;
+
+const history = createHistory();
+
+const middlewares = [thunk, routerMiddleware(history)];
+
+if (env === "development") {
+  const { logger } = require("redux-logger");
+  middlewares.push(logger);
+}
 
 const reducer = combineReducers({
-  users
+  users,
+  routing: routerReducer,
+  i18nState
 });
 
-let store = initialState =>
-  createStore(reducer, applyMiddleware(...middlewares));
+let store;
+
+if (env === "development") {
+  store = initialState =>
+    createStore(reducer, composeWithDevTools(applyMiddleware(...middlewares)));
+} else {
+  store = initialState => createStore(reducer, applyMiddleware(...middlewares));
+}
+
+export { history };
 
 export default store();
---
+++ frontend/src/translations.js
@@ -0,0 +1,5 @@
+export const translations = {
+  ko: {
+    "Login in": "로그인"
+  }
+};
