3492476aefb180dd98e5d9157c78d7af7d6bf132
Yeondam Park <giyen9088@gmail.com>
2017-12-04 21:46:48 -0800

    add routes

---
+++ frontend/src/components/App/presenter.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import styles from "./styles.scss";
+import Footer from "components/Footer";
+
+const App = props => [
+  // Nav,
+  props.isLoggedIn ? <PrivateRoutes key={2} /> : <PublicRoutes key={2} />,
+  <Footer key={3} />
+];
+
+const PrivateRoutes = props => (
+  <Switch>
+    <Route exact path="/" render={() => "feed"} />
+    <Route exact path="/explore" render={() => "explore"} />
+  </Switch>
+);
+
+const PublicRoutes = props => (
+  <Switch>
+    <Route exact path="/" render={() => "login"} />
+    <Route exact path="/forgot" render={() => "password"} />
+  </Switch>
+);
+
+export default App;
