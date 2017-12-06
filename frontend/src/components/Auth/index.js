1405e8ea7482b29c1d1ba623eb3221bb298676ab
Yeondam Park <giyen9088@gmail.com>
2017-12-05 21:12:25 -0800

    add routes

---
+++ frontend/src/components/Auth/index.js
@@ -0,0 +1,12 @@
+import { connect } from "react-redux";
+import Container from "./container";
+
+// Add all the actions for:
+// log in
+// sign up
+// Recover password
+// Check username
+// Check password
+// Check email
+
+export default connect()(Container);
---
+++ frontend/src/components/Auth/presenter.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Auth = (props, context) => <h1>Waddup</h1>;
+
+export default Auth;
