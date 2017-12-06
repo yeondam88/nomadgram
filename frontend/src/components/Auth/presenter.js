61328ddb546ba495f7a0d2f65e2a53ddd2d3d72d
Yeondam Park <giyen9088@gmail.com>
2017-12-05 22:37:26 -0800

    Sign up and login page added

--- frontend/src/components/Auth/presenter.js
+++ frontend/src/components/Auth/presenter.js
@@ -1,5 +1,59 @@
 import React from "react";
+import styles from "./styles.scss";
 
-const Auth = (props, context) => <h1>Waddup</h1>;
+const Auth = (props, context) => (
+  <main className={styles.auth}>
+    <div className={styles.column}>
+      <img src={require("images/phone.png")} alt="Checkout our app. Is cool" />
+    </div>
+    <div className={styles.column}>
+      <div className={styles.whiteBox}>
+        {(() => {
+          switch (props.action) {
+            case "login":
+              return (
+                <p>
+                  Don't have an account?{" "}
+                  <span
+                    onClick={props.changeAction}
+                    className={styles.changeLink}
+                  >
+                    Sign up
+                  </span>
+                </p>
+              );
+            case "signup":
+              return (
+                <p>
+                  Have an account?{" "}
+                  <span
+                    onClick={props.changeAction}
+                    className={styles.changeLink}
+                  >
+                    Log in
+                  </span>
+                </p>
+              );
+            default:
+              null;
+          }
+        })()}
+      </div>
+      <div className={styles.appBox}>
+        <span>Get the app</span>
+        <div className={styles.appstores}>
+          <img
+            src={require("images/ios.png")}
+            alt="Download it on the Apple Appstore"
+          />
+          <img
+            src={require("images/android.png")}
+            alt="Download it on the playstore"
+          />
+        </div>
+      </div>
+    </div>
+  </main>
+);
 
 export default Auth;
