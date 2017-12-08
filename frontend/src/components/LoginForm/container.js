80bebb9cc30cb75cada8fb79468f8aa3ed091914
Yeondam Park <giyen9088@gmail.com>
2017-12-07 22:04:39 -0800

    react-facebook-login added

--- frontend/src/components/LoginForm/container.js
+++ frontend/src/components/LoginForm/container.js
@@ -1,6 +1,40 @@
-import React from "react";
+import React, { Component } from "react";
 import LoginForm from "./presenter";
 
-const Container = props => <LoginForm {...props} />;
+class Container extends Component {
+  state = {
+    username: "",
+    password: ""
+  };
+
+  _handleInputChange = e => {
+    const { target: { value, name } } = e;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  _handleSubmit = e => {
+    e.preventDefault();
+    console.log(this.state);
+  };
+
+  _handleFacebookLogin = response => {
+    console.log(response);
+  };
+
+  render() {
+    const { username, password } = this.state;
+    return (
+      <LoginForm
+        usernameValue={username}
+        passwordValue={password}
+        handleSubmit={this._handleSubmit}
+        handleInputChange={this._handleInputChange}
+        handleFacebookLogin={this._handleFacebookLogin}
+      />
+    );
+  }
+}
 
 export default Container;
--- frontend/src/components/SignupForm/presenter.js
+++ frontend/src/components/SignupForm/presenter.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Ionicon from "react-ionicons";
 import PropTypes from "prop-types";
+import Ionicon from "react-ionicons";
 import formStyles from "shared/formStyles.scss";
 
 const SignupForm = (props, context) => (
@@ -9,39 +9,65 @@ const SignupForm = (props, context) => (
       {context.t("Sign up to see photos and videos from your friends.")}
     </h3>
     <button className={formStyles.button}>
-      <Ionicon icon="logo-facebook" fontSize="20px" color="white" />{" "}
+      <Ionicon icon="logo-facebook" fontSize="20px" color="white" />
       {context.t("Log in with Facebook")}
     </button>
-    <span className={formStyles.divider}>{context.t("or")}</span>
-    <form className={formStyles.form}>
+    <span className={formStyles.divider}>or</span>
+    <form className={formStyles.form} onSubmit={props.handleSubmit}>
       <input
         type="email"
         placeholder={context.t("Email")}
         className={formStyles.textInput}
+        value={props.emailValue}
+        onChange={props.handleInputChange}
+        name="email"
       />
       <input
         type="text"
         placeholder={context.t("Full Name")}
         className={formStyles.textInput}
+        value={props.fullNameValue}
+        onChange={props.handleInputChange}
+        name="fullName"
       />
       <input
         type="username"
         placeholder={context.t("Username")}
         className={formStyles.textInput}
+        value={props.usernameValue}
+        onChange={props.handleInputChange}
+        name="username"
       />
       <input
         type="password"
         placeholder={context.t("Password")}
         className={formStyles.textInput}
+        value={props.passwordValue}
+        onChange={props.handleInputChange}
+        name="password"
       />
-      <input type="submit" value="Sign up" className={formStyles.button} />
+      <input
+        type="submit"
+        value={context.t("Sign up")}
+        className={formStyles.button}
+        onChange={props.handleInputChange}
+      />
     </form>
     <p className={formStyles.terms}>
-      {context.t("By Signing up, you agree to our")}{" "}
+      {context.t("By signing up, you agree to our ")}
       <span>{context.t("Terms & Privacy Policy")}</span>.
     </p>
   </div>
 );
+
+SignupForm.propTypes = {
+  emailValue: PropTypes.string.isRequired,
+  fullNameValue: PropTypes.string.isRequired,
+  usernameValue: PropTypes.string.isRequired,
+  passwordValue: PropTypes.string.isRequired,
+  handleInputChange: PropTypes.func.isRequired,
+  handleSubmit: PropTypes.func.isRequired
+};
 
 SignupForm.contextTypes = {
   t: PropTypes.func.isRequired
