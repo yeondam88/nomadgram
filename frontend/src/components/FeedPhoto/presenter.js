d38a863167ccee5c98feb46468062a929946d58f
Yeondam Park <giyen9088@gmail.com>
2017-12-11 21:28:45 -0800

    Feed component added

---
+++ frontend/src/components/FeedPhoto/presenter.js
@@ -0,0 +1,44 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styles from "./styles.scss";
+
+const FeedPhoto = (props, context) => {
+  return (
+    <div className={styles.feedPhoto}>
+      <header>
+        <img
+          src={props.creator.profile_image || require("images/noPhoto.jpg")}
+          alt={props.creator.username}
+        />
+        <div>
+          <span>{props.creator.username}</span>
+          <span>{props.location}</span>
+        </div>
+      </header>
+      <img src={props.file} alt={props.caption} />
+    </div>
+  );
+};
+
+FeedPhoto.propTypes = {
+  creator: PropTypes.shape({
+    profile_image: PropTypes.string,
+    username: PropTypes.string.isRequired
+  }).isRequired,
+  location: PropTypes.string.isRequired,
+  file: PropTypes.string.isRequired,
+  like_count: PropTypes.number.isRequired,
+  caption: PropTypes.string.isRequired,
+  comments: PropTypes.arrayOf(
+    PropTypes.shape({
+      message: PropTypes.string.isRequired,
+      creator: PropTypes.shape({
+        profile_image: PropTypes.string,
+        username: PropTypes.string.isRequired
+      }).isRequired
+    })
+  ).isRequired,
+  created_at: PropTypes.string.isRequired
+};
+
+export default FeedPhoto;
--- frontend/src/redux/modules/photos.js
+++ frontend/src/redux/modules/photos.js
@@ -1,8 +1,19 @@
 // imports
 
+import { actionCreators as userActions } from "redux/modules/user";
+
 // actions
 
+const SET_FEED = "SET_FEED";
+
 // action creator
+
+function setFeed(feed) {
+  return {
+    type: SET_FEED,
+    feed
+  };
+}
 
 // api actions
 
@@ -14,8 +25,14 @@ function getFeed() {
         Authorization: `JWT ${token}`
       }
     })
-      .then(response => response.json())
-      .then(json => console.log(json));
+      .then(response => {
+        if (response.status === 401) {
+          dispatch(userActions.logout());
+        } else {
+          return response.json();
+        }
+      })
+      .then(json => dispatch(setFeed(json)));
   };
 }
 
@@ -27,12 +44,23 @@ const initialState = {};
 
 function reducer(state = initialState, action) {
   switch (action.type) {
+    case SET_FEED:
+      return applySetFeed(state, action);
     default:
       return state;
   }
 }
 
 // reducer function
+
+function applySetFeed(state, action) {
+  const { feed } = action;
+
+  return {
+    ...state,
+    feed
+  };
+}
 
 // exports
 
